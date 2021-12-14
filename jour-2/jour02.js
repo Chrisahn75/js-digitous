e8b1f22b0e54fed55aa9c1816a4cff4e691e85e8
Chrisahn75 <chris.ahona@gmail.com>
2021-12-14 15:19:54 +0100

    first commit

---
+++ jour-2/jour02.js
@@ -0,0 +1,50 @@
+// 01 Number
+let integer = 102;
+let float = 13.9;
+console.log(integer,float);
+
+// 02 Convert
+let basic = 34;
+let stringfield = basic;
+
+console.log(stringfield);
+
+// 03 Round 
+let num = 1.5;
+let rounded = Math.round(num);
+
+console.log(rounded);
+
+// 04 Aritmétique 
+let test = 12;
+let bis = 5;
+
+console.log(test + bis);
+console.log(test - bis);
+console.log(test * bis);
+console.log(test / bis);
+console.log(test ** bis);
+console.log(test % bis);
+
+// 05 Comparaison
+var teste = 143;
+var biss = 219;
+
+console.log(teste > biss);
+console.log(teste < biss);
+console.log(teste >= biss);
+console.log(teste <= biss);
+console.log(teste !== biss);
+console.log(teste === biss);
+
+// 06 Condition
+let limit = 89;
+let score = 64;
+
+if (score >= limit){
+    console.log("Ok good !")
+} else {
+    console.log("Oh nooo ...")
+}
+
+// 07 Condition
