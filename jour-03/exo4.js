e96923f5205f92a2c8cc5d206264a7798928d144
Chrisahn75 <chris.ahona@gmail.com>
2021-12-15 15:46:08 +0100

    difficile

---
+++ jour-03/exo4.js
@@ -0,0 +1,8 @@
+// 04 Order
+const numbers = [4,10,8,12,6];
+
+numbers.reverse();
+console.log(numbers);
+
+numbers.sort((a, b) => a - b);
+console.log(numbers);
---
+++ jour-03/exo6.js
@@ -0,0 +1,8 @@
+// 06 Reverse
+
+let sentence = ["Hello Konexio !"];
+ 
+for(let i = sentence - 1; i >= sentence.length; i--){
+    console.log(sentence);
+}
+
