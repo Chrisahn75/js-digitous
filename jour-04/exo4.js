2de731dcfcf478349242eea75a0c5491c2deeaef
Chrisahn75 <chris.ahona@gmail.com>
2021-12-16 15:39:47 +0100

    exo

--- jour-04/exo4.js
+++ jour-04/exo4.js
@@ -1,7 +1,16 @@
 // Compare 
 
 function compare(num1,num2){
-    for (let i = 0; i <= 4; i++){
-        
+    for (let i = 1; i <= 1; i++){
+        if (num1 === num2){
+            console.log("both are the same");
+        } else if (i = 8 === num1){
+            console.log("num1 is bigger");
+        } else if (i = 4 === num2){
+            console.log("num2 is bigger");
+        } else {
+            console.log[i];
+        }
     }
 }
+compare();
