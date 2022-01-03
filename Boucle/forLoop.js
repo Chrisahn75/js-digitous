15b818d49ffe273ddb3d83ce32ce2ff890120d09
Chrisahn75 <chris.ahona@gmail.com>
2022-01-03 13:53:40 +0100

    difficulté

---
+++ Boucle/forLoop.js
@@ -0,0 +1,16 @@
+// 1 Somme des carrés
+
+
+// 2 
+
+var result = userLength * basePrice;
+if (result % patternLength) {
+    var remainder =  result % patternLength;
+    if (remainder >= patternLength / 2)
+        result += patternLength - remainder;
+    else
+        result -= remainder;
+
+}
+
+result = Math.round(result * 100) / 100;
