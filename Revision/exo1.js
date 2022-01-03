c6747c039df932982541709d9ccd3fe533f846fc
Chrisahn75 <chris.ahona@gmail.com>
2022-01-03 14:23:59 +0100

    compliquer

---
+++ Revision/exo1.js
@@ -0,0 +1,29 @@
+// 1 Alphabet
+//const sortLetters = "Konexio";
+//let result = "";
+
+//for (let i = sortLetters.length - 1; i >= 0; i--) {
+//	result += sortLetters[i];
+//}
+
+//console.log(result);
+
+function sortLetters(s) {
+    let letters = "Konexio";
+    let res = "";
+    s += "<"; 
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "<") {
+            res += letters[i];
+        } else {
+            let start = i;
+            while (s[i] === ">") i++;
+            for (let j = i; j >= start; j--) {
+                res += letters[j]; // descending series of letters
+            }
+        }
+    }
+    return res;
+}
+
+console.log(letters);
