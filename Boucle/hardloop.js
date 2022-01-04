0ec5fff4e5732ef6811bfdfe46f3c304b699e8d6
Chrisahn75 <chris.ahona@gmail.com>
2022-01-04 12:29:45 +0100

    exo

---
+++ Boucle/hardloop.js
@@ -0,0 +1,30 @@
+
+const tab = ["hello", "goodbye", "yes", "no", "stop", "go go go"];
+
+function splitWordsAddInTab(words) {
+	if (!Array.isArray(words)) {
+		console.log("We need an array of strings here!");
+		return null;
+	}
+	let word = "";
+	let numberO = 0;
+
+	for (let i = 0; i < words.length; i++) {
+		if (!(typeof words[i] === "string")) {
+			console.log("The array should contain only strings!");
+			return null;
+		}
+
+		word = words[i].split("");
+
+		for (let j = 0; j < word.length; j++) {
+			if (word[j] === "o") {
+				numberO += 1;
+			}
+		}
+	}
+
+	return numberO;
+}
+
+console.log(splitWordsAddInTab(tab));
