df019eaef55d66804461e8d42694c5c978b09a6a
Chrisahn75 <chris.ahona@gmail.com>
2022-01-05 16:41:32 +0100

    exo

--- jour-05/exo2.js
+++ jour-05/exo2.js
@@ -1,5 +1,11 @@
 // 2 
 
-function multiply (){
+function multiply (n){
+    for(let i = 1; i <= 10; ++i){
+        let res = n * i;
+       //console.log(res);
+       console.log(`${i} * ${n} = ${res}`);
 
+    }
 }
+multiply(parseInt(process.argv[2]));
