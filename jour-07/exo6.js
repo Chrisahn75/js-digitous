99bfc422a816e14f217d621dc7faeec467349c79
Chrisahn75 <chris.ahona@gmail.com>
2022-01-10 16:11:23 +0100

    exo6

---
+++ jour-07/exo6.js
@@ -0,0 +1,45 @@
+// 6
+let cakes = 
+[
+	{
+		name: "cake",
+		flavor: "vanilla",
+		status: "available"
+	},
+	{
+		name: "brownie",
+		flavor: "chocolate",
+		status: "available"
+	},
+	{
+		name: "pie",
+		flavor: "strawberry",
+		status: "available"
+	},
+	{
+		name: "muffin",
+		flavor: "pistachio",
+		status: "available"
+	},
+	{
+		name: "donut",
+		flavor: "chocolate",
+		status: "available"
+	},
+]
+
+
+let sold = cakes.filter(function (chocolat) {
+    return chocolat.flavor === "chocolate";
+});
+
+console.log(sold);
+
+const statut = cakes.map((change) => ({
+    ... change,
+    status: cakes.status = "sold out !",
+    
+
+}));
+
+console.log(statut);
