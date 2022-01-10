03192b8fe4cdfa0b9636910504e1a695354fe69f
Chrisahn75 <chris.ahona@gmail.com>
2022-01-10 14:31:37 +0100

    exo3

--- jour-07/exo3.js
+++ jour-07/exo3.js
@@ -1,1 +1,16 @@
 // 3
+let longNames = [
+	{
+		firstName: "Jane",
+		lastName: "Doe"
+	},
+	{
+		firstName: "John",
+		lastName: "Smith"
+	}
+];
+
+let shortNames = longNames.map(function(longNames){
+	return `${"name"} : ${longNames.firstName} ${longNames.lastName}`;
+});
+console.log(shortNames);
