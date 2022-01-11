58bd5f079ccf9a5f12424d11a85931fdcc7534c5
Chrisahn75 <chris.ahona@gmail.com>
2022-01-11 16:21:02 +0100

    exo2

---
+++ jour-08/exo2.js
@@ -0,0 +1,9 @@
+const axios = require ("axios");
+
+function getFact(){
+    axios.get("https://api.chucknorris.io/jokes/random").then((res) => {
+    console.log(res.data);
+        
+    });
+}
+getFact();
